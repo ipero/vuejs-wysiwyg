3f918aa8f0b26a41931d75b65f214f61c50cdc1d
ipero <ipero@live.com>
2016-09-13 14:10:08 -0500

    update

--- app.js
+++ app.js
@@ -1,58 +1,71 @@
 
-// Vue.component('wysiwyg-editor', {
-//   template:
-//       '<div id="editor-menu">'
-//       + '<button class="mdl-button mdl-js-button mdl-button--icon" @click="bold">'
-//         + '<i class="material-icons">format_bold</i>'
-//       + '</button>'
-//       + '<button class="mdl-button mdl-js-button mdl-button--icon" @click="italic">'
-//         + '<i class="material-icons">format_italic</i>'
-//       + '</button>'
-//       + '<button class="mdl-button mdl-js-button mdl-button--icon" @click="underline">'
-//         + '<i class="material-icons">format_underlined</i>'
-//       + '</button>'
-//
-//       + '<button class="mdl-button mdl-js-button mdl-button--icon" @click="unorderedList">'
-//         + '<i class="material-icons">format_list_bulleted</i>'
-//       + '</button>'
-//       + '<button class="mdl-button mdl-js-button mdl-button--icon" @click="orderedList">'
-//         + '<i class="material-icons">format_list_numbered</i>'
-//       + '</button>'
-//       + '<!-- headings  button-->'
-//       + '<button id="headings" class="mdl-button mdl-js-button">'
-//         + '<span>Headings</span>'
-//       + '</button>'
-//
-//       + '<ul class="mdl-menu mdl-menu--bottom-left mdl-js-menu mdl-js-ripple-effect"'
-//           + 'for="headings">'
-//         + '<li class="mdl-menu__item" @click="heading(1)">Heading 1</li>'
-//         + '<li class="mdl-menu__item" @click="heading(2)">Heading 2</li>'
-//         + '<li class="mdl-menu__item" @click="heading(3)">Heading 3</li>'
-//         + '<li class="mdl-menu__item" @click="heading(4)">Heading 4</li>'
-//         + '<li class="mdl-menu__item" @click="heading(5)">Heading 5</li>'
-//         + '<li class="mdl-menu__item" @click="heading(6)">Heading 6</li>'
-//       + '</ul>'
-//     + '</div>'
-//     + '<div >'
-//       + '<iframe id="editor"></iframe>'
-//       + '<div id="html-editor">'
-//         + 'HTML Editor'
-//       + '<textarea id="textarea_id" v-model="textarea" @keyup="updateWysiwyg">Start writing ...</textarea>'
-//       + '</div>'
-//
-//   + '</div>'
-// });
+
+Vue.component('wysiwyg-editor', {
+  template:
+      `<div id='editor-menu'>
+        <button class='mdl-button mdl-js-button mdl-button--icon' @click='bold'>
+          <i class='material-icons'>format_bold</i>
+        </button>
+        <button class='mdl-button mdl-js-button mdl-button--icon' @click='italic'>
+          <i class='material-icons'>format_italic</i>
+        </button>
+        <button class='mdl-button mdl-js-button mdl-button--icon' @click='underline'>
+          <i class='material-icons'>format_underlined</i>
+        </button>
+
+        <button class='mdl-button mdl-js-button mdl-button--icon' @click='unorderedList'>
+          <i class='material-icons'>format_list_bulleted</i>
+        </button>
+        <button class='mdl-button mdl-js-button mdl-button--icon' @click='orderedList'>
+          <i class='material-icons'>format_list_numbered</i>
+        </button>
+        <button class='mdl-button mdl-js-button mdl-button--icon' @click='addLink'>
+          <i class='material-icons'>insert_link</i>
+        </button>
+        <!-- headings  button-->
+        <button id='headings' class='mdl-button mdl-js-button'>
+          <span>Headings</span>
+        </button>
+
+        <ul class='mdl-menu mdl-menu--bottom-left mdl-js-menu mdl-js-ripple-effect'
+            for='headings'>
+          <li class='mdl-menu__item' @click='heading(1)'>Heading 1</li>
+          <li class='mdl-menu__item' @click='heading(2)'>Heading 2</li>
+          <li class='mdl-menu__item' @click='heading(3)'>Heading 3</li>
+          <li class='mdl-menu__item' @click='heading(4)'>Heading 4</li>
+          <li class='mdl-menu__item' @click='heading(5)'>Heading 5</li>
+          <li class='mdl-menu__item' @click='heading(6)'>Heading 6</li>
+        </ul>
+
+        <button class='mdl-button mdl-js-button mdl-button--icon' @click='clearFormat'>
+          <i class='material-icons'>format_clear</i>
+        </button>
+      </div>
+      <div >
+        <iframe id='editor'></iframe>
+        <div id='html-editor'>
+          HTML Editor
+          <textarea id='textarea_id' v-model='textarea' @keyup='updateWysiwyg'>Start writing ...</textarea>
+        </div>
+
+      </div>`
+});
 
 new Vue({
   el: '#text-editor',
-  data: {
-    textarea: '...'
+  data: function(){
+    return{
+      textarea: '...'
+    }
+
+  },
+  filters: {
+    marked: marked
   },
 
   ready: function(){
 
     var frame = document.getElementById('editor');
-
     // is it IE
     var isIE = /*@cc_on!@*/false;
 
@@ -61,12 +74,12 @@ new Vue({
     this.frameDoc.open();
     this.frameDoc.write('<html><head></head><body>'+ this.textarea +'</body></html>');
     this.frameDoc.close();
-    this.frameDoc.designMode = "on";
+    this.frameDoc.designMode = 'on';
 
     if(this.frameDoc.addEventListener) {
         this.frameDoc.addEventListener('DOMSubtreeModified',  this.updateTextarea,false);
     } else if(this.frameDoc.attachEvent) {
-        //frameDoc.attachEvent("IE Event",this.updateTextarea);
+        //frameDoc.attachEvent('IE Event',this.updateTextarea);
     }
 
   },
@@ -81,12 +94,12 @@ new Vue({
       this.frameDoc.open();
       this.frameDoc.write('<html><head></head><body>'+ this.textarea +'</body></html>');
       this.frameDoc.close();
-      this.frameDoc.designMode = "on";
+      this.frameDoc.designMode = 'on';
 
       if(this.frameDoc.addEventListener) {
           this.frameDoc.addEventListener('DOMSubtreeModified',  this.updateTextarea,false);
       } else if(this.frameDoc.attachEvent) {
-          //this.frameDoc.attachEvent("IE Event",this.updateTextarea);
+          //this.frameDoc.attachEvent('IE Event',this.updateTextarea);
       }
 
     },
